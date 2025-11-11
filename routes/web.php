965b5ec2f0ec8a26c7849aeaf03e5319acda565c
tasniaanwer <tasniaanwer@gmail.com>
2025-11-11 17:56:42 +0600

    Add internship office portal functionality

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,11 @@
 <?php
 
-use App\Http\Controllers\NinjaController;
 use App\Http\Controllers\InternshipController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/ninjas', [NinjaController::class, 'index'])->name('ninjas.index');
-Route::get('/ninjas/create', [NinjaController::class, 'create'])->name('ninjas.create');
-Route::get('/ninjas/{ninja}', [NinjaController::class, 'show'])->name('ninjas.show');
-Route::post('/ninjas', [NinjaController::class, 'store'])->name('ninjas.store');
-Route::delete('/ninjas/{ninja}', [NinjaController::class, 'destroy'])->name('ninjas.destroy');
 
 Route::get('/internships', [InternshipController::class, 'index'])->name('internships.index');
 Route::get('/internships/create', [InternshipController::class, 'create'])->name('internships.create');
